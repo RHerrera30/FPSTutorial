912a4115f850c07d98a56126a7cfa99d32ee3758
RHerrera30 <152225484+RHerrera30@users.noreply.github.com>
2025-03-16 18:09:43 -0700

    Weapon and Bullets Added

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+    public float bulletVelocity = 30;
+    public float bulletPrefabLifeTime = 3f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //left-click to shoot
+        if (Input.GetMouseButtonDown(0))
+        {
+            FireWeapon();
+        }
+    }
+
+    private void FireWeapon()
+    {
+        //Instantiate the bullet
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.position, Quaternion.identity);
+        
+        //SHOOTIN'!
+        bullet.GetComponent<Rigidbody>().AddForce(bulletSpawn.forward.normalized * bulletVelocity, ForceMode.Impulse);
+        
+        //Destroy the bullet
+        StartCoroutine(DestroyBulletAfterTime(bullet, bulletPrefabLifeTime));
+    }
+
+    private IEnumerator DestroyBulletAfterTime(GameObject bullet, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Destroy(bullet);
+    }
+}
